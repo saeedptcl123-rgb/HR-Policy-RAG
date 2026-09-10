e997e22cd069f0d7c95cf82e45899a59c654f807
saeedptcl123-rgb <saeedptcl123@gmail.com>
2026-09-10 17:54:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ st.markdown(
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 
 # The requested Groq model
-GROQ_MODEL = "openai/gpt-oss-200"
+GROQ_MODEL ="openai/gpt-oss-120b""
 
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 150
